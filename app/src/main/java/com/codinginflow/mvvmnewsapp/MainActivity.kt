3f85fa46c496e0c62d2bd695f6d1b2c03291c671
Shouda Ren <renshouda@oppo.com>
2022-05-30 11:28:28 +0800

    mainActivity frame finished, bind data. integrate Frag and activitiy

--- app/src/main/java/com/codinginflow/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmnewsapp/MainActivity.kt
@@ -2,13 +2,17 @@ package com.codinginflow.mvvmnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
+import com.codinginflow.mvvmnewsapp.databinding.ActivityMainBinding
 import com.codinginflow.mvvmnewsapp.features.bookmarks.BookmarksFragment
 import com.codinginflow.mvvmnewsapp.features.breakingnews.BreakingNewsFragment
 import com.codinginflow.mvvmnewsapp.features.searchnews.SearchNewsFragment
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
+
+    // A:生成的绑定类将布局变量与布局中的视图关联起来。 这是为了通过这个方式来控制数据
+    private lateinit var binding: ActivityMainBinding
 
     //    使用 lateinit 关键字，变量在定义时不需要初始化
     private lateinit var breakingNewsFragment: BreakingNewsFragment
@@ -25,15 +29,18 @@ class MainActivity : AppCompatActivity() {
 
     //模块选择
     private var selectedIndex = 0
-    private val selecteFragment get() = fragments[selectedIndex]
+    //选中的哪个frag
+    private val selectedFragment get() = fragments[selectedIndex]
 
 
     //利用 FragmentManager 类提供的方法，您可以在运行时为 Activity 添加、移除和替换 Fragment，从而营造出动态的用户体验。
     private fun selectedFragment(selectFragment: Fragment) {
         //在 Activity 的生命周期内更改 Fragment
         var transaction = supportFragmentManager.beginTransaction();
+        //index 一共就3个 正好对应
         fragments.forEachIndexed { index, fragment ->
             if (selectFragment == fragment) {
+                //改变的fragment重新附上
                 transaction = transaction.attach(fragment)
                 selectedIndex = index
             } else {
@@ -41,12 +48,23 @@ class MainActivity : AppCompatActivity() {
             }
         }
         transaction.commit();
+        // 随着点击不同的导航栏的项目 更改界面小标题
+        title = when (selectedFragment) {
+            is BreakingNewsFragment -> getString(R.string.title_breaking_news)
+            is SearchNewsFragment -> getString(R.string.title_search_news)
+            is BookmarksFragment -> getString(R.string.title_bookmarks)
+            else -> ""
+        }
 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //A: 绑定类的 inflate() 方法来扩充视图层次结构并将对象绑定到该层次结构 绑定的是ActivityMainXML里的数据
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 //            如果之前没有fragment
         if (savedInstanceState == null) {
             breakingNewsFragment = BreakingNewsFragment()
@@ -69,11 +87,34 @@ class MainActivity : AppCompatActivity() {
             //如果为null取0
             selectedIndex = savedInstanceState.getInt(KEY_SELECTED_INDEX, 0)
         }
+        //动态体验
+        selectedFragment(selectedFragment)
 
-        selectedFragment(selecteFragment)
+        //A: binding的使用,bottomNav 在mainACtiv里
+        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                //检测导航栏中哪个选项被触发
+                R.id.breaking_icon -> breakingNewsFragment
+                R.id.search_icon -> searchingNewsFragment
+                R.id.bookmarks_icon -> bookmarksFragment
+                else -> throw IllegalArgumentException("unexpected error")
+            }
+            //动态体验
+            selectedFragment(fragment)
+            true
+        }
 
     }
+    //为了防止在search 和bookmark framgment的后退时直接退出应用
+    override fun onBackPressed() {
+        if (selectedIndex != 0) {
+            //直接回到breaking fragment
+            binding.bottomNav.selectedItemId = R.id.breaking_icon
+        } else {
+            super.onBackPressed()
+        }
 
+    }
     //把新的selectedIndex储存到InstanceState （ Bundle）里
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
